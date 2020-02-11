68a7d67c8b27763868f3e08224aa90e55cc99acc
Wil Wilsman <wil@wilwilsman.com>
2020-02-11 13:04:32 -0600

    :bug: Use run command to run percy
    Required with NPM, optional but accepted with Yarn

--- test.js
+++ test.js
@@ -25,47 +25,47 @@ describe('onPostBuild', () => {
   it('calls `percy snapshot` with npm using the public path', () => {
     onPostBuild({ store }, {})
     expect(execSyncMock.calls).toHaveLength(1)
-    expect(execSyncMock.calls[0][0]).toBe(`${npm} percy snapshot public`)
+    expect(execSyncMock.calls[0][0]).toBe(`${npm} run percy snapshot public`)
   })
 
   it('calls `percy snapshot` with yarn using the public path', () => {
     program.useYarn = true
     onPostBuild({ store }, {})
     expect(execSyncMock.calls).toHaveLength(1)
-    expect(execSyncMock.calls[0][0]).toBe(`${yarn} percy snapshot public`)
+    expect(execSyncMock.calls[0][0]).toBe(`${yarn} run percy snapshot public`)
   })
 
   it('calls `percy snapshot` with the resolved public path', () => {
     program.directory = 'some/project/path'
     onPostBuild({ store }, {})
     expect(execSyncMock.calls).toHaveLength(1)
-    expect(execSyncMock.calls[0][0]).toBe(`${npm} percy snapshot some/project/path/public`)
+    expect(execSyncMock.calls[0][0]).toBe(`${npm} run percy snapshot some/project/path/public`)
   })
 
   it('calls `percy snapshot` with the `snapshot-files` option', () => {
     onPostBuild({ store }, { files: '*.html' })
     expect(execSyncMock.calls).toHaveLength(1)
-    expect(execSyncMock.calls[0][0]).toBe(`${npm} percy snapshot public --snapshot-files *.html`)
+    expect(execSyncMock.calls[0][0]).toBe(`${npm} run percy snapshot public --snapshot-files *.html`)
 
     onPostBuild({ store }, { files: ['*.html', '*.htm'] })
     expect(execSyncMock.calls).toHaveLength(2)
-    expect(execSyncMock.calls[1][0]).toBe(`${npm} percy snapshot public --snapshot-files *.html,*.htm`)
+    expect(execSyncMock.calls[1][0]).toBe(`${npm} run percy snapshot public --snapshot-files *.html,*.htm`)
   })
 
   it('calls `percy snapshot` with the `ignore-files` option', () => {
     onPostBuild({ store }, { ignore: '*.html' })
     expect(execSyncMock.calls).toHaveLength(1)
-    expect(execSyncMock.calls[0][0]).toBe(`${npm} percy snapshot public --ignore-files *.html`)
+    expect(execSyncMock.calls[0][0]).toBe(`${npm} run percy snapshot public --ignore-files *.html`)
 
     onPostBuild({ store }, { ignore: ['*.html', '*.htm'] })
     expect(execSyncMock.calls).toHaveLength(2)
-    expect(execSyncMock.calls[1][0]).toBe(`${npm} percy snapshot public --ignore-files *.html,*.htm`)
+    expect(execSyncMock.calls[1][0]).toBe(`${npm} run percy snapshot public --ignore-files *.html,*.htm`)
   })
 
   it('calls `percy snapshot` with the `config` option', () => {
     onPostBuild({ store }, { config: 'path/to/.percy.yml' })
     expect(execSyncMock.calls).toHaveLength(1)
-    expect(execSyncMock.calls[0][0]).toBe(`${npm} percy snapshot public --config path/to/.percy.yml`)
+    expect(execSyncMock.calls[0][0]).toBe(`${npm} run percy snapshot public --config path/to/.percy.yml`)
   })
 
   it('calls `percy snapshot` with multiple options', () => {
@@ -80,7 +80,7 @@ describe('onPostBuild', () => {
 
     expect(execSyncMock.calls).toHaveLength(1)
     expect(execSyncMock.calls[0][0]).toBe([
-      `${yarn} percy snapshot some/project/path/public`,
+      `${yarn} run percy snapshot some/project/path/public`,
       '--snapshot-files *.html',
       '--ignore-files *.htm',
       '--config path/to/.percy.yml'
